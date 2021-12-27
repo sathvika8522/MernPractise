ab6ce02c8cd01ae7684b9d6ebd03c73d630107c8
sathvika <sathvika8522@gmail.com>
2021-12-28 00:10:54 +0530

    connected to mongodb

---
+++ app.js
@@ -0,0 +1,26 @@
+const exp=require('express')
+const app=exp()
+const mongoose=require('mongoose')
+const dotenv=require('dotenv')
+
+dotenv.config({path:'./config.env'});
+const dburl=process.env.DATABASEURL;
+
+mongoose.connect(dburl, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+.then(() => {
+    console.log("Successfully connected to mongo.");
+})
+.catch((err) => {
+    console.log("Error connecting to mongo.", err);
+});
+
+
+app.get('/', (req,res)=>
+{
+   res.send('om namah shivaya')
+})
+
+app.listen(3000,()=>{console.log('connected.........')})
